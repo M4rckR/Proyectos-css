0732e9088f038c99ccbd90941e24b689ca68fe1e
Marcos Romero <-e>
2023-10-22 18:23:00 -0500

    Estudio de Springboot: Inicio seguro

--- 3.- Proyectos personales/2.- Proyectos Universidad/demo/src/main/java/com/example/demo/web/ControladorREST.java
+++ 3.- Proyectos personales/2.- Proyectos Universidad/demo/src/main/java/com/example/demo/web/ControladorREST.java
@@ -1,11 +1,8 @@
 package com.example.demo.web;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
